c08aa4668d777eb90156847d3f1baaaca7790631
David Friberg <david.friberg@zenseact.com>
2020-12-22 11:00:45 +0100

    Doom config update

---
+++ emacs/.doom.d/custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" "4bca89c1004e24981c840d3a32755bf859a6910c65b829d9441814000cf6c3d0" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "f2927d7d87e8207fa9a0a003c0f222d45c948845de162c885bf6ad2a255babfd" "99ea831ca79a916f1bd789de366b639d09811501e8c092c85b2cb7d697777f93" "e074be1c799b509f52870ee596a5977b519f6d269455b84ed998666cf6fc802a" "188fed85e53a774ae62e09ec95d58bb8f54932b3fd77223101d036e3564f9206" "990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" "f7216d3573e1bd2a2b47a2331f368b45e7b5182ddbe396d02b964b1ea5c5dc27" "37144b437478e4c235824f0e94afa740ee2c7d16952e69ac3c5ed4352209eefb" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ccls-sem-global-variable-face ((t (:underline t :weight extra-bold))))
+ '(lsp-face-highlight-read ((t (:background "sea green"))))
+ '(lsp-face-highlight-write ((t (:background "brown4"))))
+ '(lsp-ui-peek-highlight ((t (:background "deep sky blue"))))
+ '(lsp-ui-sideline-current-symbol ((t (:foreground "grey38" :box nil))))
+ '(lsp-ui-sideline-symbol ((t (:foreground "grey30" :box nil)))))
